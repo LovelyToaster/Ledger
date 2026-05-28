c83645a3f160777386dfc6de02886ba2d7c2e4c7
Lovel <lovelytoaster94@outlook.com>
2026-05-28 23:18:06 +0800

    chore: 版本号 1.4.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.verdantgem.ledger"
         minSdk = 34
         targetSdk = 36
-        versionCode = 12
-        versionName = "1.4.3"
+        versionCode = 13
+        versionName = "1.4.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
